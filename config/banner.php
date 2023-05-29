b3b3df0aaf14b25a54383b9cf0f8c94ea54552f1
Reza Moradi <54495069+rmoradi2019@users.noreply.github.com>
2023-05-30 01:01:54 +0330

    Add files via upload

---
+++ config/banner.php
@@ -0,0 +1,9 @@
+<?php
+    return [
+        'language' => 'fa-IR', // Zaban (Default = fa-IR)English = en-US - Turkish = tr-TR - Persian (Farsi) = fa-IR //
+        'template' => 'light', // Template Dark (dark) , Light (light) //
+        'admin_server_group_id' => '538', // Group ID Admin Ha Dar Configton //
+        'support_server_group_id' => '539', // Group ID Support Ha Dar Configton //
+        'cache' => false, // Cache Beshe Ya Na ? (default = true) //
+        'cache_ttl' => 30, // Time Clear Cache ? (default = 30) //
+    ];
